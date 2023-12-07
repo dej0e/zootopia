c98defd7270dd90f174619a60bbfbb7fe497a4ac
dej0e <dejoejohn@gmail.com>
2023-12-07 13:22:33 -0500

    Adds Dummy ReportsController

--- src/main/java/com/teamOne/cs631/controller/MainController.java
+++ src/main/java/com/teamOne/cs631/controller/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j2;
 import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxWeaver;
@@ -27,6 +28,8 @@ public class MainController {
     public Button openRevenueTypesDialog;
     @FXML
     public Button openRevenueEventsDialog;
+    @FXML
+    public Button openReportsDialog;
 
     public MainController(FxWeaver fxWeaver) {
         this.fxWeaver = fxWeaver;
@@ -94,6 +97,21 @@ public class MainController {
                         FxControllerAndView<AnimalDialogController, VBox> tiledDialog =
                                 fxWeaver.load(AnimalDialogController.class);
                         tiledDialog.getController().show();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        showAlert(e.getLocalizedMessage());
+                    }
+                }
+        );
+
+        openReportsDialog.setOnAction(
+                actionEvent -> {
+                    try {
+                        FxControllerAndView<ReportsController, VBox> tiledDialog =
+                                fxWeaver.load(ReportsController.class);
+                        Stage stage = (Stage) openReportsDialog.getScene().getWindow();
+                        stage.setScene(tiledDialog.getView().get().getScene());
+
                     } catch (Exception e) {
                         e.printStackTrace();
                         showAlert(e.getLocalizedMessage());
